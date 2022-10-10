51f3d8e2a25871fc8e26992b9f55c2c1ac81e6ca
Julia N'Diamoi <ndiamoij@gmail.com>
2022-10-10 18:58:48 +0100

    playing around with const and let

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,33 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+const firstName = "Tanya";
+const age = 10;
+
+const nameAndAge = firstName + age;
+
+let a = 5;
+let b = 10;
+console.log(a+b); // 15
+
+a = 20;
+console.log(a+b); //30
+
+var c = a + b;
+console.log(c); //30
+
+a = 3;
+console.log(c); //30
+
+c = a + b + b;
+
+console.log(c) // 23
+
+b = 1;
+
+console.log(a +b); // 4
+
+const d = c + a;
+console.log(d); //26
+
+d = 5; //will cause error
+
