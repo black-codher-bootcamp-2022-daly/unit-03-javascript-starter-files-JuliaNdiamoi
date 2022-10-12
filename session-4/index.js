d72a251cf5813aca4d22e05c44db9f3114ec98a3
Julia N'Diamoi <ndiamoij@gmail.com>
2022-10-12 19:59:36 +0100

    mathmetical operations

--- session-4/index.js
+++ session-4/index.js
@@ -53,24 +53,34 @@ console.log(biography(personA));
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y " + division);
+
+const IsEqual = x == y;
+console.log("Is it equal? " + IsEqual);
+
+const increment = x++;
+console.log("Increment x " + increment); 
+
+const decrement = y--;
+console.log("decrement y " + decrement); 
+
 
 
 
